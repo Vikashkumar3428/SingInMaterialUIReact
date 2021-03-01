3b639d9b6d1dab64fbd598494e49a279ec891356
Vikashkumar3428 <vkumar3428@gmail.com>
2021-03-02 01:03:02 +0530

    Sign In form Frontend Part complete

--- src/login.jsx
+++ src/login.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import { Grid,Paper,Avatar } from "@material-ui/core";
+import { Grid,Paper,Avatar,Button, TextField, Checkbox ,FormControlLabel,Link, Typography} from "@material-ui/core";
 import LockOpenOutlined from '@material-ui/icons/LockOpenOutlined'
 
 
 const Login =()=>{
     const paperStyle= { padding: 20, height:'60vh',width: '35vh' ,margin:"5vw auto"}
     const avatarStyle= {backgroundColor:"#1bbd7e"}
+    const btnStyle= {margin:"10px 0"}
 return(
 
     <Grid>
@@ -13,8 +14,31 @@ return(
         <Grid align="center">
             <Paper elevation ={10} style={paperStyle}>
                 <Avatar style={avatarStyle}><LockOpenOutlined/></Avatar>
-                Sign in
-             </Paper>
+                <h2>Sign in</h2>
+                <TextField style={{float:'left'}} fullWidth required label="Username" placeholder= "Enter Your Name here...."/>
+                <TextField style={{float:'left'}} fullWidth required  label="Password" type="Password" placeholder= "Enter Your Password....."/>
+                <FormControlLabel style={{float:'left'}} 
+                    control={
+                    <Checkbox
+                        name="checkedB"
+                        color="primary"
+                    />
+                    }
+                    label="Remember Me!"
+                />
+                <Button type="Submit" color="primary" variant="contained" style={btnStyle} fullWidth>Sign In</Button>
+                <Typography  style={{float:'left'}} >  
+                    <Link href="forgotpassword" fullWidth required>
+                        Forgot Password ?
+                    </Link>
+                    </Typography>
+                <Typography style={{float:'left'}} fullWidth required> Have an account ?
+                    <Link href="signup">
+                        Sign Up Here
+                    </Link>
+                </Typography>
+            </Paper>
+             
         </Grid>
 
     </Grid>
